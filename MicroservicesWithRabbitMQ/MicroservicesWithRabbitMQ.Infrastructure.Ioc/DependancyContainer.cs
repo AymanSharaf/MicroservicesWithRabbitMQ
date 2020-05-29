acf523812f7d139e3d0c2514130f1f78dce94c40
AymanAbdelaal <ayman.s.ismaeel@gmail.com>
2020-05-30 00:08:28 +0300

    Initial commit of Dependancy Container

---
+++ MicroservicesWithRabbitMQ/MicroservicesWithRabbitMQ.Infrastructure.Ioc/DependancyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicroservicesWithRabbitMQ.Domain.Core.Bus;
+using MicroservicesWithRabbitMQ.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroservicesWithRabbitMQ.Infrastructure.Ioc
+{
+    public class DependancyContainer
+    {
+        public static void RegisterServices(IServiceCollection services) 
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
